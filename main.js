fb773d82d881b230e5120fb3d12dbe41d8f6bc0b
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2022-01-26 18:58:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function home()
+{
+    window.location="index.html";
+}
+function playzone()
+{
+    window.location="play.html";
+}
+function storyzone()
+{
+    window.location="story.html";
+}
+function rhymezone()
+{
+    window.location="rhyme.html";
+}
+function studyzone()
+{
+    window.location="study.html";
+}
