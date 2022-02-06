200a8b453cfcb037a32d30fc73bdaf7de32311bd
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2022-02-06 16:52:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,11 @@ function studyzone()
 {
     window.location="study.html";
 }
+var width=screen.width;
+newwidth=screen.width-70;
+newheight=screen.height-300;
+if(width<992)
+{
+    document.getElementById("f1").width=newwidth;
+    document.getElementById("f1").height=newheight;
+}
